8250e25f1573af04339bd1d569ce5eaced31dcca
Kyyyu <mrizkii254500@gmail.com>
2026-02-12 18:22:19 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: 'index.html',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
